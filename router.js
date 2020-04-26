ff8ce6cea8541003e99df71d0e4149eba82a5b1c
Kevin Qi <iqnivek@gmail.com>
2020-04-25 18:03:11 -0700

    create /wishlists endpoint

--- router.js
+++ router.js
@@ -1,9 +1,39 @@
 const express = require("express");
 
+const knex = require("./db/knex");
+
 const router = express.Router();
 
-router.get("/health", (req, res) => {
-  return res.status(200).json({ status: "ok" });
+router.get("/wishlists", async (req, res) => {
+  try {
+    const wishlists = await knex("wishlists").select("*");
+    const wishlistProducts = await knex("wishlist_products")
+      .join("products", "products.id", "wishlist_products.product_id")
+      .select("*")
+      .whereIn(
+        "wishlist_products.wishlist_id",
+        wishlists.map((item) => item.id)
+      )
+      .orderBy("products.id");
+
+    const productMap = wishlistProducts.reduce((map, item) => {
+      map[item.wishlist_id] = map[item.wishlist_id] || [];
+      map[item.wishlist_id].push(item);
+      return map;
+    }, {});
+
+    const results = wishlists.map((wishlist) => {
+      return {
+        ...wishlist,
+        products: productMap[wishlist.id],
+      };
+    });
+
+    return res.status(200).json({ data: results });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: "failed to retrieve wishlists" });
+  }
 });
 
 module.exports = router;
